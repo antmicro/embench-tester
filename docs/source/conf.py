b73a97d177738f932ae4723ad7e484889b922ee0
Maciej Dudek <mdudek@internships.antmicro.com>
2020-11-06 13:56:40 +0100

    Change html theme form default to sphinx_antmicro_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_antmicro_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -40,6 +41,7 @@ release = ''
 # ones.
 extensions = [
     'sphinx.ext.githubpages',
+    'sphinx_antmicro_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -75,7 +77,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_antmicro_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
