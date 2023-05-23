64a1c44ef9cd702a4419dda64fa576fd704294a6
Antoni Pokusinski <apokusinski@internships.antmicro.com>
2023-05-23 15:18:54 +0200

    Get yosys and verilator from conda

--- install.sh
+++ install.sh
@@ -23,22 +23,6 @@ quilt push -a
 
 cd third_party
 
-# yosys install
-cd yosys
-make config-gcc
-echo -n "PREFIX := $BASE_DIR/env/conda/envs/embench-tester" >> Makefile.conf
-make -j $(nproc)
-make install
-cd ..
-
-#verilator install
-cd verilator
-autoconf
-./configure --prefix=$BASE_DIR/env/conda/envs/embench-tester
-make -j $(nproc)
-make install
-cd ..
-
 # ghdl download and install
 cd ghdl
 ./configure --prefix=$BASE_DIR/env/conda/envs/embench-tester
